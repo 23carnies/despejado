4bd200925de739c2e81bfa47dd75f0929f601751
23carnies <46233990+23carnies@users.noreply.github.com>
2020-12-06 11:44:42 -0500

    Display WeatherCard in F

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -6,8 +6,8 @@ const WeatherCard = ({ weatherData }) => {
         <div>
             <h3>Weather for {weatherData.name}</h3>
             <h4>Current Weather: {weatherData.weather.main}</h4>
-            <h4>Current Temperature: {weatherData.main.temp}</h4>
-            <h5>Feels like: {weatherData.main.feels_like}</h5>
+            <h4>Current Temperature: {((weatherData.main.temp - 273.15) * 9/5 + 32).toFixed(0) + `°F`}</h4>
+            <h5>Feels like: {((weatherData.main.feels_like - 273.15) * 9/5 + 32).toFixed(0) + `°F`}</h5>
             <h4>Humidity: {weatherData.main.humidity}</h4>
             <h4>Wind: {weatherData.wind.speed}</h4>
             <h4>Sunrise: {weatherData.sys.sunrise}</h4>
